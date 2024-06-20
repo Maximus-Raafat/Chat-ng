2ba350aef0d25b745d5950b4a046fa4bdad79c88
Maximus <79494898+Maximus-Raafat@users.noreply.github.com>
2024-06-20 12:15:02 +0300

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
     {
-        path:"Chat-ng/chat",
+        path:"chat",
         loadComponent: () => import('./pages/chat/chat.component').then((com)=>
         com.ChatComponent),
         canActivate : [authGuard]
